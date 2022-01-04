90f885a88fffeb11d926e8ae20c639f8f3f4e3f1
Ginko Balboa <ginkobalboa3@gmail.com>
2022-01-04 12:27:10 +0100

    Fix for Linux

--- src/parfis/global.cpp
+++ src/parfis/global.cpp
@@ -14,7 +14,7 @@ std::string parfis::Global::currentDateTime()
     time_t     now = time(0);
     struct tm  tstruct;
     char       buf[80];
-    localtime_s(&tstruct, &now);
+    tstruct = *localtime(&now);
     strftime(buf, sizeof(buf), "%Y-%m %d.%X", &tstruct);
     return buf;
 }
